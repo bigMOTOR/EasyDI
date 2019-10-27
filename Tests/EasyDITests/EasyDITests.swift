0922ddae2ac282efa868d100f521cdddae63fced
Nikolay Fiantsev <to.bigmotor@gmail.com>
2019-10-27 21:51:10 +0200

    Add tests

--- Tests/EasyDITests/EasyDITests.swift
+++ Tests/EasyDITests/EasyDITests.swift
@@ -1,15 +1,83 @@
 import XCTest
 @testable import EasyDI
 
-final class EasyDITests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(EasyDI().text, "Hello, World!")
-    }
+private let _helloWorld = "Hello, World!"
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+final class EasyDITests: XCTestCase {
+  private var container: Container!
+
+  override func setUp() {
+    super.setUp()
+    container = Container()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+  }
+  
+  func testRegisterAndResolveObject() {
+    container.register(factory: { TestClass() })
+    container.object(TestClass.self, implements: ProtocolA.self)
+    let object: ProtocolA = try! container.resolve()
+    XCTAssertTrue(object is TestClass)
+    XCTAssertEqual(object.someValue, _helloWorld)
+  }
+  
+  func testRegisterAndResolveObjectForConcatenateType() {
+    container.register(factory: { TestClass() })
+    container.object(TestClass.self, implements: ProtocolA.self)
+    container.object(TestClass.self, implements: ProtocolB.self)
+    let object: ProtocolA & ProtocolB = try! container.resolve()
+    XCTAssertTrue(object is TestClass)
+    XCTAssertEqual(object.someValue, _helloWorld)
+    XCTAssertEqual(object.someValue2, "ProtocolB")
+  }
+  
+  func testUniqueScope() {
+    container.register(factory: { TestClass() })
+    let object1: TestClass = try! container.resolve()
+    object1.someValue = _helloWorld + _helloWorld
+    let object2: TestClass = try! container.resolve()
+    XCTAssertEqual(object1.someValue, _helloWorld + _helloWorld)
+    XCTAssertEqual(object2.someValue, _helloWorld )
+    XCTAssertFalse(object1 === object2)
+  }
+  
+  func testWeakSingletonScope() {
+    container.register(factory: { TestClass() }, scope: .weakSingleton)
+    container.object(TestClass.self, implements: ProtocolA.self)
+    let object1: TestClass = try! container.resolve()
+    object1.someValue = _helloWorld + _helloWorld
+    let object2: TestClass = try! container.resolve()
+    XCTAssertEqual(object1.someValue, _helloWorld + _helloWorld)
+    XCTAssertEqual(object2.someValue, _helloWorld + _helloWorld )
+    XCTAssertTrue(object1 === object2)
+  }
+  
+  static var allTests = [
+    ("testRegisterAndResolveObject", testRegisterAndResolveObject),
+    ("testRegisterAndResolveObjectForConcatenateType", testRegisterAndResolveObjectForConcatenateType),
+    ("testUniqueScope", testUniqueScope),
+    ("testWeakSingletonScope", testWeakSingletonScope)
+  ]
 }
+
+private protocol ProtocolA {
+  var someValue: String { get set }
+}
+
+private class TestClass: ProtocolA {
+  var someValue: String = "Hello, World!"
+}
+
+private protocol ProtocolB {
+  var someValue2: String { get }
+}
+
+extension ProtocolB where Self: TestClass {
+  var someValue2: String {
+    return "ProtocolB"
+  }
+}
+
+extension TestClass: ProtocolB {}
