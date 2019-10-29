0b2ce8d0a1f00800d3f4e6194650b0b55ff94500
Nikolay Fiantsev <to.bigmotor@gmail.com>
2019-10-29 17:09:26 +0200

    Add scope help

--- Sources/EasyDI/Scope.swift
+++ Sources/EasyDI/Scope.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 public enum Scope {
+  /// resolve your type as a new instance every time you call resolve
   case unique
+  /// container stores week reference to the resolved instance. While a strong reference to the resolved instance exists resolve will return the same instance. After the resolved instance is deallocated next resolve will produce a new instance.
   case weakSingleton
 }
 
